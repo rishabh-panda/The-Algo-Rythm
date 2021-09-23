c9b437e9300230d42b38110c09e2c34fc19db5f3
Rishabh Panda (He/Him) <80598737+rishabh-panda@users.noreply.github.com>
2021-09-23 23:13:51 +0530

    Update ARRAY_TridiagonalMatrix.c

--- ARRAY_TridiagonalMatrix.c
+++ ARRAY_TridiagonalMatrix.c
@@ -9,7 +9,8 @@ int mat[5][5]={{81,82,83,84,65},
                {11,12,13,14,15},
                {16,17,18,19,20},
                {21,22,23,24,25}};
-  
+
+// Display of Matrix
 printf("Entered matrix is:\n\n");
 for(i=0;i<5;i++)
     {
